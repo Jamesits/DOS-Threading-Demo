4fab8a23def1274494f82538e71bb5d8e184e8fb
James Swineson <i@swineson.me>
2016-10-31 19:52:51 +0800

    more comments

--- dosroot/src/thread.c
+++ dosroot/src/thread.c
@@ -8,6 +8,7 @@
 /* 0x1C is usable */
 #define TIME_INT 0x1C
 #define NTCB 32
+#define TCB_NAME_LEN 32
 #define GET_INDOS 0x34
 #define GET_CRIT_ERR 0x5d06
 enum THREAD_STATUS {FINISHED, RUNNING, READY, BLOCKED};
@@ -18,7 +19,7 @@ typedef struct TCB {
     unsigned ss;                /* stack segment */
     unsigned sp;                /* thread in-stack offset */
     enum THREAD_STATUS state;
-    char name[32];
+    char name[TCB_NAME_LEN];
 } s_TCB;
 
 /* stack initializer */
@@ -34,8 +35,8 @@ s_TCB tcb[NTCB];
 int tcb_count = 0;
 void interrupt (*oldtimeslicehandler)(void);
 int ss, sp, cs, ds;
-char far *indos_ptr=0;  /*该指针变量存放INDOS标志的地址*/
-char far *crit_err_ptr=0;  /*该指针变量存放严重错误标志的地址*/
+char far *indos_ptr = 0;  /*该指针变量存放INDOS标志的地址*/
+char far *crit_err_ptr = 0;  /*该指针变量存放严重错误标志的地址*/
 
 /* function declaration */
 int create(char *name, func thread_function, size_t stacklen);
@@ -144,6 +145,7 @@ int create(char *name, func thread_function, size_t stacklen) {
     regs.es = FP_SEG(tcb[tcb_count].stack);
     regs.seg = FP_SEG(thread_end_trigger);
     regs.off = FP_OFF(thread_end_trigger);
+    regs.flags = 200;
     memcpy(tcb[tcb_count].stack, &regs, sizeof(regs));
     strcpy(tcb[tcb_count].name, name);
     ++tcb_count;
@@ -221,41 +223,42 @@ void interrupt timeslicehandler(void) {
 /* InitDos()函数：功能是获得INDOS标志的地址和严重错误标志的地址 */
 void InitDos(void)
 {
-  union REGS regs;
-  struct SREGS segregs;
+    union REGS regs;
+    struct SREGS segregs;
 
-  /* 获得 INDOS 标志的地址 */
-  regs.h.ah=GET_INDOS;
-  /* intdosx() ：Turbo C的库函数，其功能是调用DOS的INT21H中断*/
-  intdosx(&regs,&regs,&segregs);
-  /* MK_FP()：不是一个函数，只是一个宏。*/
-/*其功能是做段基址加上偏移地址的运算，也就是取实际地址。 */
-  indos_ptr=MK_FP(segregs.es,regs.x.bx);
+    /* 获得 INDOS 标志的地址 */
+    regs.h.ah=GET_INDOS;
+    /* intdosx() ：Turbo C的库函数，其功能是调用DOS的INT21H中断*/
+    intdosx(&regs,&regs,&segregs);
+    /* MK_FP()：不是一个函数，只是一个宏。*/
+    /*其功能是做段基址加上偏移地址的运算，也就是取实际地址。 */
+    indos_ptr=MK_FP(segregs.es,regs.x.bx);
 
-  /*获得严重错误标志的地址 */
-  /*代码中用到的_osmajor、_osminor是Turbo C的全程变量，其中前者为*/
-/*DOS版本号的主要部分，后者为版本号的次要部分。*/
-  if (_osmajor<3)
-    crit_err_ptr=indos_ptr+1;
-  else if (_osmajor==3 && _osminor==0)
-    crit_err_ptr=indos_ptr-1;
-  else {
+    /*获得严重错误标志的地址 */
+    /*代码中用到的_osmajor、_osminor是Turbo C的全程变量，其中前者为*/
+    /*DOS版本号的主要部分，后者为版本号的次要部分。*/
+    if (_osmajor<3) {
+        crit_err_ptr=indos_ptr+1;
+    } else if (_osmajor==3 && _osminor==0) {
+        crit_err_ptr=indos_ptr-1;
+    } else {
     regs.x.ax=GET_CRIT_ERR;
     intdosx(&regs,&regs,&segregs);
     crit_err_ptr=MK_FP(segregs.ds,regs.x.si);
   }
 }
 
- /* DosBusy()：函数功能是获得Indos标志及严重错误标志的值，判断是否dos忙：*/
+/* DosBusy()：函数功能是获得Indos标志及严重错误标志的值，判断是否dos忙：*/
 /* 如果返回值是1，表示dos忙；*/
- /* 如果返回值是0，表示dos不忙；*/
- /* 如果返回值是-1，表示还没有调用InitDos() */
+/* 如果返回值是0，表示dos不忙；*/
+/* 如果返回值是-1，表示还没有调用InitDos() */
 int DosBusy(void)
 {
-  if (indos_ptr && crit_err_ptr)
-    return(*indos_ptr || *crit_err_ptr);
-  else
-    return(-1);    /* InitDos() hasn't been called */
+    if (indos_ptr && crit_err_ptr) {
+        return (*indos_ptr || *crit_err_ptr);
+    } else {
+        return(-1);    /* InitDos() hasn't been called */
+    }
 }
 
 
