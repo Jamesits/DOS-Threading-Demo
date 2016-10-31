ff2fa0e546630ee4e0d1a54270bc74dcf90a9e8c
James Swineson <i@swineson.me>
2016-10-31 14:14:43 +0800

    destory

--- dosroot/src/thread.c
+++ dosroot/src/thread.c
@@ -31,32 +31,29 @@ int create(char *name, func thread_function, size_t stacklen) {
     tcb[tcb_count].sp = FP_OFF(tcb[tcb_count].stack);
     tcb[tcb_count].state = READY;
     strcpy(tcb[tcb_count].name, name);
-    ++tcb_count;
+    return tcb_count++;
 };
 
-int destroy();
+int destroy(int id) {
+    if (id >= NTCB) {
+        printf("Cannot destory thread #%d", id);
+        return -1;
+    }
+    free(tcb[id].stack);
+    memcpy(tcb + id * sizeof(s_TCB), tcb + (id + 1) * sizeof(s_TCB), (NTCB - id - 1) * sizeof(s_TCB));
+    --tcb_count;
+    return 0;
+};
 
 void interrupt (*oldtimeslicehandler)(void);
-
-int fuck = 0;
-
 void interrupt timeslicehandler(void) {
-    fuck++;
+    printf("interrupt\n");
 }
 
 int main() {
     int i = 32767;
     oldtimeslicehandler = getvect(0x1C);
     setvect(0x1C, timeslicehandler);
-    while (i--) {printf("%d\n", fuck);};
     setvect(0x1C, oldtimeslicehandler);
-    return 0;
-}
-
-int create(char *name, codeptr code, int stck) {
-    tcb_count++;
-    tcb[tcb_count].stack = malloc(stck);
-    /* memcpy(tcb[tcb_count].stack, struct int_regs, sizeof(struct int_regs)); */
-
     return 0;
 }
